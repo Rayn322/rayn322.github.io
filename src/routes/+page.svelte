f1a106f201be6f898ff7067fbc6da584c6620412
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-10-25 17:57:20 -0700

    Clone the old site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<h1>RyanLand is very fun</h1>
+	<h1>Click the button below for more fun (it is lots of fun)</h1>
+	<a href="fun">FUN!</a>
+</div>
+
+<style>
+	div {
+		margin: 4em auto;
+		padding: 0 2em;
+		text-align: center;
+	}
+
+	h1 {
+		margin-bottom: 0.5em;
+	}
+
+	a {
+		background-color: #4caf50;
+		color: white;
+		padding: 1em 2em;
+		text-decoration: none;
+		display: inline-block;
+	}
+</style>
